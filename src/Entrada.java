5bb29991d54e062345dc04b3729682bc23d821e2
JavierGasparinoMunoz <javier.gasparino@cesjuanpablosegundo.es>
2020-01-24 14:51:37 +0100

    Initial commit

---
+++ src/Entrada.java
@@ -0,0 +1,8 @@
+import ventanas.VentanaSplash;
+
+public class Entrada {
+    public static void main(String[] args) {
+        VentanaSplash ventanaSplash = new VentanaSplash();
+        ventanaSplash.mostrar();
+    }
+}
---
+++ src/controladoras/ControladoraVentanaSplash.java
@@ -0,0 +1,79 @@
+package controladoras;
+
+import javafx.animation.FadeTransition;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladoraVentanaSplash implements Initializable {
+
+    @FXML
+    ImageView imagenFondo;
+
+    @FXML
+    ProgressBar barraProgreso;
+
+    Task tareaSecundaria;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+          instancias();
+
+        barraProgreso.progressProperty().bind(tareaSecundaria.progressProperty());
+
+        FadeTransition transition = new FadeTransition(Duration.seconds(3),imagenFondo);
+        transition.setFromValue(0.0);
+        transition.setToValue(1.0);
+        transition.play();
+
+        transition.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                new Thread(tareaSecundaria).start();
+            }
+        });
+
+        /*tareaSecundaria.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent event) {
+                try {
+                     ventanaUno = new VentanaUno();
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Stage stage = (Stage) imagenFondo.getScene().getWindow();
+                stage.close();
+            }
+        });*/
+
+    }
+
+    private void instancias() {
+        tareaSecundaria = new Task() {
+            @Override
+            protected Object call() throws Exception {
+
+                for(int i = 0;i < 100;i++){
+                    updateProgress(i,100);
+                    Thread.sleep(100);
+                }
+
+                updateProgress(1,100);
+
+                return null;
+            }
+        };
+    }
+}
